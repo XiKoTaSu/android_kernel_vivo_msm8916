e261c97eeb4f3e503c827edde93ea4b8f80847b4
sheikhshahnawaz41299 <sheikhshahnawaz5555@gmail.com>
2022-04-07 23:55:43 +0530

    drivers/i2c/busses: vivo changes for i2c suspended

--- drivers/i2c/busses/i2c-msm-v2.c
+++ drivers/i2c/busses/i2c-msm-v2.c
@@ -149,6 +149,9 @@ static void i2c_msm_qup_fifo_calc_size(struct i2c_msm_ctrl *ctrl)
 			fifo->input_fifo_sz, fifo->output_fifo_sz);
 
 }
+/* vivo add */
+int qup_i2c_suspended = 0;  
+EXPORT_SYMBOL_GPL(qup_i2c_suspended); 
 
 /*
  * i2c_msm_tag_byte: accessor for tag as four bytes array
@@ -2724,6 +2727,7 @@ static int i2c_msm_pm_sys_suspend_noirq(struct device *dev)
 		pm_runtime_set_suspended(dev);
 		pm_runtime_enable(dev);
 	}
+	qup_i2c_suspended = 1;
 
 	return ret;
 }
@@ -2739,6 +2743,9 @@ static int i2c_msm_pm_sys_resume_noirq(struct device *dev)
 	mutex_lock(&ctrl->xfer.mtx);
 	ctrl->pwr_state = I2C_MSM_PM_RT_SUSPENDED;
 	mutex_unlock(&ctrl->xfer.mtx);
+
+	qup_i2c_suspended = 0;
+
 	return  0;
 }
 #endif
